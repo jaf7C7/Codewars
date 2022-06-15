43270cc1d8625af0103227add2b37dce39f5287a
Joss <jossappletonfox@protonmail.com>
2022-06-15 11:09:22 +0100

    Add solution `Find The Smallest Integer'

---
+++ javascript/8th_kyu/smallestInteger.js
@@ -0,0 +1,25 @@
+// Find The Smallest Integer
+// 
+// Given an array of integers your solution should find the smallest integer.
+// 
+// For example:
+// 
+//     Given [34, 15, 88, 2] your solution will return 2
+//     Given [34, -345, -1, 100] your solution will return -345
+// 
+// You can assume, for the purpose of this kata, that the supplied array
+// will not be empty.
+
+class SmallestIntegerFinder {
+	findSmallestInt(args) {
+		return args.reduce((a, b) => a < b ? a : b);
+	}
+}
+
+// Tests
+let sif = new SmallestIntegerFinder;
+console.log(sif.findSmallestInt([78,56,232,12,8]) == 8);
+console.log(sif.findSmallestInt([78,56,232,12,18]) == 12);
+console.log(sif.findSmallestInt([78,56,232,412,228]) == 56);
+console.log(sif.findSmallestInt([78,56,232,12,0]) == 0);
+console.log(sif.findSmallestInt([1,56,232,12,8]) == 1);
