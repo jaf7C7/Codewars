93bb1982a2f54a2f7b14f1bdfd5a1703b5a03271
Joss <jossappletonfox@protonmail.com>
2022-06-15 13:47:39 +0100

    Add solution `String Repeat'

---
+++ javascript/8th_kyu/stringRepeat.js
@@ -0,0 +1,26 @@
+/*
+ * String Repeat
+ * 
+ * Source: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/javascript
+ * 
+ * Write a function called repeatStr which repeats the given string string
+ * exactly n times.
+ * 
+ * repeatStr(6, "I") // "IIIIII"
+ * repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+ */
+
+function repeatStr(n, str) {
+	let result = '';
+	while (result.length < n * str.length) {
+		result += str;
+	}
+	return result;
+}
+
+// Tests
+console.log(repeatStr(6, "I") == "IIIIII");
+console.log(repeatStr(5, "Hello") == "HelloHelloHelloHelloHello");
+console.log(repeatStr(3, "*") == "***");
+console.log(repeatStr(5, "#") == "#####");
+console.log(repeatStr(2, "ha ") == "ha ha ");
